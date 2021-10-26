922ffd87ea445e06810395331bf994c8914e1d1f
miaolei <17611555590@163.com>
2021-10-26 15:32:33 +0800

    修改readme.md

---
+++ src/main/java/com/zhouzifei/cache/FileCacheEngine.java
@@ -0,0 +1,106 @@
+package com.zhouzifei.cache;
+
+import com.zhouzifei.cache.common.SerializationUtils;
+import com.zhouzifei.cache.util.FileManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * 文件存储换成
+ * @author 周子斐 (17600004572@163.com)
+ *
+ */
+public class FileCacheEngine implements CacheEngine {
+
+	private  static final FileManager FILE_MANAGER;
+	static
+	{
+		FILE_MANAGER = new FileManager();
+		FILE_MANAGER.init();
+	}
+	@Override
+	public void add(String cacheName, Serializable key, Object value) {
+		if(cacheName!=null&&key!=null&&value!=null){		
+			try {
+				byte[] data = SerializationUtils.serialize(value);
+				FILE_MANAGER.add(cacheName, key, data);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	@Override
+	public void add( Serializable key, Object value) {
+		add(FOLDER_NAME, key, value);
+	}
+	@Override
+	public Object get(String folderName, Serializable key) {
+		if(folderName!=null&&key!=null)
+		{
+			try {
+				byte[] data = FILE_MANAGER.get(folderName, key);
+				return SerializationUtils.deserialize(data);
+			} catch (IOException e) {			
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	@Override
+	public Object get( Serializable key) {		
+		return get(FOLDER_NAME, key);
+	}
+
+	@Override
+	public void remove(String folderName, Serializable key) {
+		if(folderName!=null&&key!=null)
+		{
+			FILE_MANAGER.remove(folderName, key);
+		}
+		
+	}
+	@Override
+	public void remove(Serializable key) {
+		remove(FOLDER_NAME, key);
+		
+	}
+	@Override
+	public void clear(String folderName) {
+		if(folderName!=null)
+		{
+			try {
+				FILE_MANAGER.clear(folderName);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	@Override
+	public void clear() {
+		clear(FOLDER_NAME);
+		
+	}
+	@Override
+	public void stop() {
+		try {
+			FILE_MANAGER.close();
+		} catch (IOException e) {		
+			e.printStackTrace();
+		}
+		
+	}
+	public File[] getList(){
+		return getList(FOLDER_NAME);
+	}
+	public File[] getAll(){
+		return getList();
+	}
+	public File[] getList(String cacheName){
+		return FILE_MANAGER.getList(cacheName);
+	}
+
+}
